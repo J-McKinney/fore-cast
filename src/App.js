29d247c82363689f0f2a8c1bb0cca1aeedfdfda9
Jesse <jessemckinney513@gmail.com>
2020-11-09 15:52:32 -0500

    yay

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    forcast: {},
+  };
+  componentDidMount() {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=Atlanta,Georgia&units=imperial&appid=166a433c57516f51dfab1f7edaed8413"
+    )
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            forcast: result,
+          });
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  }
+
+  componentDidUpdate() {
+    var tellMeTheForecast = this.state.forecast;
+    console.log("Forecast   ", this.state.forcast);
+    document.getElementById("App").innerHTML = tellMeTheForecast;
+  }
+  render() {
+    return (
+      <>
+        <div id="App" className="App"></div>
+      </>
+    );
+  }
 }
 
 export default App;
