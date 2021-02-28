bef91490b3735a0b195e7577d75a07548292c0d7
Jesse <jessemckinney513@gmail.com>
2021-02-28 12:37:27 -0500

    yay it works

--- src/App.js
+++ src/App.js
@@ -1,30 +1,44 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
-        </div>
-      </>
+// "https://api.openweathermap.org/data/2.5/weather?q=Atlanta,Georgia&units=imperial&appid=166a433c57516f51dfab1f7edaed8413"
+
+function App() {
+  const [currentLocation, setCurrentLocation] = useState({});
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        let coordinates = pos.coords;
+        setCurrentLocation(coordinates);
+      },
+      (err) => {
+        console.warn(`Error(${err.code}): ${err.message}`);
+      },
+      {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0,
+      }
     );
-  }
+  }, []);
+
+  return (
+    <>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <button onClick={() => console.log(currentLocation)}>
+            Show Coordinates
+          </button>
+        </header>
+      </div>
+    </>
+  );
 }
 
 export default App;
