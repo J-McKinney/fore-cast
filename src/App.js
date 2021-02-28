e2a0c4f27fb9ce7037255334a8e8bd053aeae203
Jesse <jessemckinney513@gmail.com>
2021-02-28 11:52:54 -0500

    starting over

--- src/App.js
+++ src/App.js
@@ -1,37 +1,27 @@
 import React, { Component } from "react";
+import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
-  state = {
-    forcast: {},
-  };
-  componentDidMount() {
-    fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=Atlanta,Georgia&units=imperial&appid=166a433c57516f51dfab1f7edaed8413"
-    )
-      .then((res) => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            forcast: result,
-          });
-        },
-        (error) => {
-          console.log(error);
-        }
-      );
-  }
-
-  componentDidUpdate() {
-    console.log("This is the Forecast   ", this.state.forcast);
-    var tellMeTheForecast = this.state.forecast;
-    document.getElementById("App").innerHTML = tellMeTheForecast;
-  }
-
   render() {
     return (
       <>
-        <div id="App" className="App" />
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
       </>
     );
   }
