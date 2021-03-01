f34954af2f52f027875db1523b192fad2d4add04
Jesse <jessemckinney513@gmail.com>
2021-03-01 15:22:12 -0500

    yay

--- src/App.js
+++ src/App.js
@@ -3,60 +3,61 @@ import logo from "./logo.svg";
 import "./App.css";
 require("dotenv").config();
 
-const GKey = process.env.REACT_APP_GKEY;
-const WKey = process.env.REACT_APP_WKEY;
+// const GKey = process.env.REACT_APP_GKEY;
+// const WKey = process.env.REACT_APP_WKEY;
 
 function App() {
-  const [currentLocation, setCurrentLocation] = useState({});
+  // const [currentLocation, setCurrentLocation] = useState({});
 
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      (pos) => {
-        let coordinates = pos.coords;
-        setCurrentLocation(coordinates);
-      },
-      (err) => {
-        console.warn(`Error(${err.code}): ${err.message}`);
-      },
-      {
-        enableHighAccuracy: true,
-        timeout: 5000,
-        maximumAge: 0,
-      }
-    );
-  }, []);
+  // useEffect(() => {
+  //   navigator.geolocation.getCurrentPosition(
+  //     (pos) => {
+  //       let coordinates = pos.coords;
+  //       setCurrentLocation(coordinates);
+  //       console.log(coordinates)
+  //     },
+  //     (err) => {
+  //       console.warn(`Error(${err.code}): ${err.message}`);
+  //     },
+  //     {
+  //       enableHighAccuracy: true,
+  //       timeout: 5000,
+  //       maximumAge: 0,
+  //     }
+  //   );
+  // }, []);
 
-  useEffect(() => {
-    async function getOneCallData(WKey, position) {
-      let lat = position.latitude;
-      let lon = position.longitude;
+  // useEffect(() => {
+  //   async function getOneCallData(WKey, position) {
+  //     let lat = position.latitude;
+  //     let lon = position.longitude;
 
-      const oneCallAPI = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=${WKey}`;
+  //     const oneCallAPI = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,alerts&appid=${process.env.REACT_APP_WKEY}`;
 
-      let res = await fetch(oneCallAPI);
-      let data = await res.json();
+  //     let res = await fetch(oneCallAPI);
+  //     let data = await res.json();
 
-      console.log("WeatherCode: " + data);
-    }
+  //     console.log("WeatherCode: " + data);
+  //   }
 
-    async function getReverseGeocodingData(GKey, position) {
-      let lat = position.latitude;
-      let lon = position.longitude;
+  //   async function getReverseGeocodingData(GKey, position) {
+  //     let lat = position.latitude;
+  //     let lon = position.longitude;
 
-      const reverseGeocodingAPI = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lon}&key=${GKey}`;
+  //     const reverseGeocodingAPI = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lon}&key=${process.env.REACT_APP_GKEY}`;
 
-      let res = await fetch(reverseGeocodingAPI);
-      let data = await res.json();
+  //     let res = await fetch(reverseGeocodingAPI);
+  //     let data = await res.json();
 
-      console.log("GeoCode: " + data);
-    }
+  //     console.log("GeoCode: " + data);
+  //   }
 
-    if (typeof currentLocation.latitude != "undefined") {
-      console.log("Hello")
-      getOneCallData(WKey.oneCall, currentLocation);
-      getReverseGeocodingData(GKey.reverseGeocoding, currentLocation)
-    }
-  }, [currentLocation]);
+  //   if (typeof currentLocation.latitude != "undefined") {
+  //     console.log();
+  //     getOneCallData(process.env.REACT_APP_WKEY.oneCall, currentLocation);
+  //     getReverseGeocodingData(process.env.REACT_APP_GKEY.reverseGeocoding, currentLocation);
+  //   }
+  // }, [currentLocation]);
 
   return (
     <>
@@ -66,9 +67,9 @@ function App() {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <button onClick={() => console.log(currentLocation)}>
+          {/* <button onClick={() => console.log(currentLocation)}>
             Show Coordinates
-          </button>
+          </button> */}
         </header>
       </div>
     </>
